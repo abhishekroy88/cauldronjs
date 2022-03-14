8a7a9f1ec0529cc1c4202a52d57a2db91cacf14a
Abhishek Roy <abhishek.roy@outlook.com>
2022-03-14 21:28:17 +0530

    setup husky/lint-staged to aid formatting during commits. testing it now.

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ import classes from './App.module.css'
 const App = () => {
   return (
     <>
-      <h1 className={classes.App}>CauldronJS</h1>
+          <h1 className={classes.App}>CauldronJS</h1>
     </>
   )
 }
