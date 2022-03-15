1534deefe5c608d0e94165bc93c54f7bc516b2df
Abhishek Roy <abhishek.roy@outlook.com>
2022-03-16 00:29:00 +0530

    separated webpack configs by env. prod merges fine with common. dev yet to be updated. css minification also missing from prod.

---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    runtime: 'regenerator-runtime/runtime.js',
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/assets/images/favicon.ico',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
