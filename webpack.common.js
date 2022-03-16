c90f6c05cca36b143508653c34e79793689bd7c0
Abhishek Roy <abhishek.roy@outlook.com>
2022-03-16 13:01:49 +0530

    setup css minification.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -24,6 +25,7 @@ module.exports = {
     splitChunks: {
       chunks: 'all',
     },
+    minimizer: [`...`, new CssMinimizerPlugin()],
   },
   module: {
     rules: [
