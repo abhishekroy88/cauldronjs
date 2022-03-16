e51c415180760ffdade47044905719332bb4e6b5
Abhishek Roy <abhishek.roy@outlook.com>
2022-03-16 12:15:47 +0530

    webpack dev config done and working fine with common.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,59 +1,10 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: {
-    runtime: 'regenerator-runtime/runtime.js',
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   devServer: {
     port: 3000,
   },
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/assets/images/favicon.ico',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(ttf|otf|woff|woff2)$/,
-        type: 'asset/resource',
-      },
-    ],
-  },
-}
+})
